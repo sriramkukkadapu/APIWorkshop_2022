9528ed7be2e5d1c117777c7f8191ff8f5fbad086
sriramkukkadapu@gmail.com <sriramkukkadapu@gmail.com>
2022-08-09 14:13:23 +0530

    first commit

---
+++ Readme.txt
@@ -0,0 +1,5 @@
+For converting Java Map to a JSON Object refer here:
+
+https://devqa.io/how-to-convert-java-map-to-json/
+
+Already we did this in Create User.java file
---
+++ src/test/java/HelloWorld.java
@@ -0,0 +1,8 @@
+import org.testng.annotations.Test;
+
+public class HelloWorld {
+    @Test
+    public void testhelloworld(){
+        System.out.println("Hello World!!!");
+    }
+}
